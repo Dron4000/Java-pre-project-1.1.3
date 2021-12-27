d81b54593606061c3315f1004f9d1a15c4221516
Dron4000 <jericho93@yandex.ru>
2021-12-27 14:07:14 +0300

    hello^-^

---
+++ Java-pre-project-1.1.3-feature-newTalibBabax/Java-pre-project-1.1.3-feature-new-TalibBabax/src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,54 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+        String name = "Andrey";
+        System.out.println(" пользователь добавлен успешно " + name);
+
+        name = "Dron";
+        userService.saveUser(name, "5000", (byte) 55);
+        System.out.println(" пользователь добавлен успешно " + name);
+
+        name = "Vitalik";
+        userService.saveUser(name, "Vitalikov", (byte) 21);
+        System.out.println(" пользователь добавлен успешно " + name);
+
+        name = "Alex";
+        userService.saveUser(name, "Pytin", (byte) 43);
+        System.out.println(" пользователь добавлен успешно " + name);
+
+        List<User> list = userService.getAllUsers();
+        for (int i = 0; i < list.size(); i++) {
+            User user = list.get(i);
+
+            System.out.println(user);
+        }
+
+
+        userService.removeUserById(2);
+
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+       Util.close();
+
+
+
+    }
+
+}
+
---
+++ Java-pre-project-1.1.3-feature-newTalibBabax/Java-pre-project-1.1.3-feature-new-TalibBabax/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,76 @@
+package jm.task.core.jdbc.util;
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import org.hibernate.service.ServiceRegistry;
+
+
+
+public class Util {
+    private  static  final String URL = "jdbc:mysql://localhost:3306/mysql?autoReconnect=true&useSSL=false" ;
+    private  static  final String USER= "root";
+    private  static  final String PASSWORD =  "rfrfzrhfcjnf93";
+
+        private static StandardServiceRegistry registry;
+        private static SessionFactory sessionFactory;
+
+        public static SessionFactory getSessionFactory() {
+
+            if (sessionFactory == null) {
+                try {
+                    Configuration configuration = new Configuration()
+                            .setProperty("hibernate.connection.url",URL)
+                            .setProperty("hibernate.connection.username",USER)
+                            .setProperty("hibernate.connection.password", PASSWORD)
+                            .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                            .addAnnotatedClass(User.class);
+
+
+                    ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                            .applySettings(configuration.getProperties()).build();
+                    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                } catch (Exception e) {
+                    e.printStackTrace();
+
+                }
+                System.out.println("Хибернет заработал");
+            }
+            return sessionFactory;
+        }
+
+
+        public static void close() {
+            if (registry != null) {
+                StandardServiceRegistryBuilder.destroy(registry);
+            }
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
